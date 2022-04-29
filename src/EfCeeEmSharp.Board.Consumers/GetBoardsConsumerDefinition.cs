c90d19f6f5fa055f7951d2714249a1f0e398e49c
Chris Meller <chris@doesnthaveone.com>
2022-04-29 07:20:28 +0300

    Add some configuration to the GetBoards consumer.

--- src/EfCeeEmSharp.Board.Consumers/GetBoardsConsumerDefinition.cs
+++ src/EfCeeEmSharp.Board.Consumers/GetBoardsConsumerDefinition.cs
@@ -11,6 +11,10 @@ public class GetBoardsConsumerDefinition : ConsumerDefinition<GetBoardsConsumer>
 
     protected override void ConfigureConsumer(IReceiveEndpointConfigurator endpointConfigurator, IConsumerConfigurator<GetBoardsConsumer> consumerConfigurator)
     {
-        
+        // configure message retry with millisecond intervals
+        endpointConfigurator.UseMessageRetry(r => r.Intervals(100,200,500,800,1000));
+
+        // use the outbox to prevent duplicate events from being published
+        endpointConfigurator.UseInMemoryOutbox();
     }
 }
